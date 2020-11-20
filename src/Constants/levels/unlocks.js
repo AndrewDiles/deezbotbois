c9e425059dff39f12a45fd1613351c86da13b75d
Andrew Diles <A_Diles@hotmail.com>
2020-11-20 07:58:46 -0500

    unlock tracker

---
+++ src/Constants/levels/unlocks.js
@@ -0,0 +1,60 @@
+// The purpose of this is to keep track of where users obtain different unlocks and to ensure the same color is not granted twice in the same slot
+
+const unlocks = {
+	acc : {
+		plateCoat: 'lv 2.3',
+		thickPain: 'start',
+		wd40: 'lv 1.1',
+
+	},
+	arm : {
+		Gun1: 'start',
+		Sword1: 'start',
+	},
+	bots : {
+		BotBoxey: 'start',
+	},
+	colors: {
+		primary: {
+			brown: 'lv 2.2',
+			khaki: 'start',
+			lime: 'start',
+		},
+		secondary: {
+			tan: 'lv 1.2',
+			tomato: 'start',
+			turquoise: 'start',
+		},
+		trim: {
+			darkkhaki: 'start',
+			green: 'start',
+			teal: 'lv 1.3',
+		},
+		extensions: {
+			silver: 'start',
+			steelblue: 'start',
+		},
+		rollers: {
+			black: 'start',
+			orchid: 'start',
+			pink: 'lv 2.1',
+		},
+		eyes: {
+			dodgerblue: 'start',
+			orange: 'start',
+		},
+		armTrim: {
+			darkmagenta: 'start',
+			steelblue: 'start',
+		},
+		armPrimary: {
+			pink: 'start',
+			silver: 'start',
+		},
+		armSecondary: {
+			deepskyblue: 'start',
+			firebrick: 'start',
+		}
+	}
+}
+export default unlocks
